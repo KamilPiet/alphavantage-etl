385e55baca6e4aa12fb50b3fbbb4450bd0cf7f7f
KamilPiet <kamil.piet@gmail.com>
2022-12-12 21:56:07 +0100

    Add global variables and type conversion in extract
    - Global variable "symbol" stores a ticker symbol of a stock, which
      price data is being processed
    - Global variable "currency" stores a currency code, to which
      the exchange rate (from USD) data is being processed
    - Type conversion (to numeric) was added in extract functions
      (get_daily_price and get_daily_exchange_rate), to ensure that
      all the data in dataframes has the correct type

--- airflow/alphavantage_etl_airflow_dag.py
+++ airflow/alphavantage_etl_airflow_dag.py
@@ -12,12 +12,14 @@ from datetime import datetime
 # extract tasks
 # extract from alphavantage and load to local postgresql database daily price (in USD) of a given symbol
 @task()
-def get_daily_price(symbol):
+def get_daily_price():
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}' \
           f'&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
     r = requests.get(url)
     data = r.json()
     df_price_usd = pd.DataFrame(data["Time Series (Daily)"]).transpose()
+    for col in df_price_usd:
+        df_price_usd[col] = pd.to_numeric(df_price_usd[col])
     conn = BaseHook.get_connection('postgres_alphavantage')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
     df_price_usd.to_sql(f'src_{symbol.lower()}_price_usd', engine, if_exists='replace', index=True)
@@ -25,24 +27,29 @@ def get_daily_price(symbol):
 
 # extract from alphavantage and load to local postgresql database daily exchange rate between USD and given currency
 @task()
-def get_daily_exchange_rate(to_currency):
-    url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol={to_currency}' \
+def get_daily_exchange_rate():
+    url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol={currency}' \
           f'&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
     r = requests.get(url)
     data = r.json()
     df_exchange_rate = pd.DataFrame(data["Time Series FX (Daily)"]).transpose()
+    for col in df_exchange_rate:
+        df_exchange_rate[col] = pd.to_numeric(df_exchange_rate[col])
     conn = BaseHook.get_connection('postgres_alphavantage')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    df_exchange_rate.to_sql(f'src_usd_{to_currency.lower()}', engine, if_exists='replace', index=True)
+    df_exchange_rate.to_sql(f'src_usd_{currency.lower()}', engine, if_exists='replace', index=True)
 
 
 with DAG(dag_id="alphavantage_etl_dag", schedule_interval="*/5 * * * *", start_date=datetime(2022, 12, 1),
          catchup=False, tags=["alphavantage"]) as dag:
 
+    symbol = "SPY"
+    currency = "PLN"
+
     with TaskGroup("extract_load_src", tooltip="Extract and load symbol price in USD and currency exchange rate") \
             as extract_load_src:
-        src_daily_price = get_daily_price("SPY")
-        src_daily_exchange_rate = get_daily_exchange_rate("PLN")
+        src_daily_price = get_daily_price()
+        src_daily_exchange_rate = get_daily_exchange_rate()
         # order
         [src_daily_price, src_daily_exchange_rate]
 
