0f3f1a51817cddad4e9d0fb8cc94a21f0129b78c
KamilPiet <kamil.piet@gmail.com>
2022-12-18 00:15:12 +0100

    Update DAG and extract tasks in alphavantage_etl_airflow_dag.py
    Now get_daily_price and get_daily_exchange_rate functions check how many
    business days are missing from a corresponding table, pull less data
    whenever possible and load, into appropriate table, only missing rows
    instead of dropping the old table and creating a new one. A new table is
    created only if it does not exist. This change was made primarily to
    reduce the duration of the extract task group.
    Furthermore, names of the first columns in source tables ware changed
    from "index" to "date" and primary keys were added to these columns.
    Rows in all tables have been reversed (compared to the previous version)
    so that the most recent data is now at the bottom of the table.
    Schedule interval of the alphavantage_etl_dag was changed from
    "every 5 minutes" to "at midnight after every business day".

--- airflow/alphavantage_etl_airflow_dag.py
+++ airflow/alphavantage_etl_airflow_dag.py
@@ -5,40 +5,110 @@ from airflow.decorators import task
 from airflow.hooks.base import BaseHook
 from airflow.models.dag import DAG
 from airflow.utils.task_group import TaskGroup
-from sqlalchemy import create_engine
-from datetime import datetime
+from sqlalchemy import create_engine, exc
+from datetime import datetime, date
 import time
+import numpy as np
 
 
 # extract tasks
 # extract from alphavantage and load to local postgresql database daily price (in USD) of a given symbol
 @task()
 def get_daily_price():
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}' \
-          f'&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
-    r = requests.get(url)
-    data = r.json()
-    df_price_usd = pd.DataFrame(data["Time Series (Daily)"]).transpose()
-    for col in df_price_usd:
-        df_price_usd[col] = pd.to_numeric(df_price_usd[col])
     conn = BaseHook.get_connection('postgres_alphavantage')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    df_price_usd.to_sql(f'src_{symbol.lower()}_price_usd', engine, if_exists='replace', index=True)
+    df_holidays = pd.read_sql_query(f'SELECT * FROM public.holidays', engine)
+    holidays = df_holidays.to_numpy(dtype='datetime64').flatten()
+    try:
+        df_recent = pd.read_sql_query(f'SELECT date FROM public.src_{symbol.lower()}_price_usd ORDER BY date DESC '
+                                      f'LIMIT 1', engine)
+        recent = datetime.strptime(df_recent.iloc[0].iat[0], '%Y-%m-%d').date()
+        date_diff = np.busday_count(recent, date.today(), holidays=holidays) - 1
+        print(f'{date_diff} business days behind')
+        if date_diff <= 1:
+            print('No need to pull data')
+            return
+        elif date_diff <= 100:
+            output_size = 'compact'
+            print('Pulling 100 rows')
+        else:
+            output_size = 'full'
+            print('Pulling all available data')
+        table_exists = True
+    except exc.ProgrammingError as e:  # to catch an exception, when the table does not exist
+        print(e)
+        output_size = 'full'
+        table_exists = False
+        print('Pulling all available data')
+    except:                            # to catch other exceptions
+        output_size = 'full'
+        table_exists = False
+        print('Pulling all available data')
+
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}' \
+          f'&outputsize={output_size}&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
+    r = requests.get(url)
+    data = r.json()
+    # convert time series to df then transpose df and reverse rows
+    df_price_usd = pd.DataFrame(data["Time Series (Daily)"]).transpose().iloc[::-1]
+
+    if table_exists:
+        df_price_usd = df_price_usd.tail(date_diff)  # keep only those rows, that are missing from the database
+    for col in df_price_usd:
+        df_price_usd[col] = pd.to_numeric(df_price_usd[col])
+
+    df_price_usd.to_sql(f'src_{symbol.lower()}_price_usd', engine, if_exists='append', index=True, index_label='date')
+    if not table_exists:
+        with engine.connect() as con:
+            con.execute(f'ALTER TABLE src_{symbol.lower()}_price_usd ADD PRIMARY KEY (date);')
 
 
 # extract from alphavantage and load to local postgresql database daily exchange rate between USD and given currency
 @task()
 def get_daily_exchange_rate():
-    url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol={currency}' \
-          f'&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
-    r = requests.get(url)
-    data = r.json()
-    df_exchange_rate = pd.DataFrame(data["Time Series FX (Daily)"]).transpose()
-    for col in df_exchange_rate:
-        df_exchange_rate[col] = pd.to_numeric(df_exchange_rate[col])
     conn = BaseHook.get_connection('postgres_alphavantage')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    df_exchange_rate.to_sql(f'src_usd_{currency.lower()}', engine, if_exists='replace', index=True)
+    try:
+        df_recent = pd.read_sql_query(f'SELECT date FROM public.src_usd_{currency.lower()} ORDER BY date DESC '
+                                      f'LIMIT 1', engine)
+        recent = datetime.strptime(df_recent.iloc[0].iat[0], '%Y-%m-%d').date()
+        date_diff = np.busday_count(recent, date.today()) - 1
+        print(f'{date_diff} business days behind')
+        if date_diff <= 1:
+            print('No need to pull data')
+            return
+        elif date_diff <= 100:
+            output_size = 'compact'
+            print('Pulling 100 rows')
+        else:
+            output_size = 'full'
+            print('Pulling all available data')
+        table_exists = True
+    except exc.ProgrammingError as e:  # to catch an exception, when the table does not exist
+        print(e)
+        output_size = 'full'
+        table_exists = False
+        print('Pulling all available data')
+    except:                            # to catch other exceptions
+        output_size = 'full'
+        table_exists = False
+        print('Pulling all available data')
+
+    url = f'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=USD&to_symbol={currency}' \
+          f'&outputsize={output_size}&apikey={os.getenv("ALPHAVANTAGE_API_KEY")}'
+    r = requests.get(url)
+    data = r.json()
+    # convert time series to df, then transpose df and reverse rows
+    df_exchange_rate = pd.DataFrame(data["Time Series FX (Daily)"]).transpose().iloc[::-1]
+
+    if table_exists:
+        df_exchange_rate = df_exchange_rate.tail(date_diff)  # keep only those rows, that are missing from the database
+    for col in df_exchange_rate:
+        df_exchange_rate[col] = pd.to_numeric(df_exchange_rate[col])
+    df_exchange_rate.to_sql(f'src_usd_{currency.lower()}', engine, if_exists='append', index=True, index_label='date')
+    if not table_exists:
+        with engine.connect() as con:
+            con.execute(f'ALTER TABLE src_usd_{currency.lower()} ADD PRIMARY KEY (date);')
 
 
 # transform and load task
@@ -50,8 +120,8 @@ def calc_load_daily_price_other_ccy():
     time.sleep(5)  # wait 5 seconds to allow database to update tables after extract tasks
     conn = BaseHook.get_connection('postgres_alphavantage')
     engine = create_engine(f'postgresql://{conn.login}:{conn.password}@{conn.host}:{conn.port}/{conn.schema}')
-    df_price_usd = pd.read_sql_query(f'SELECT * FROM public.src_{symbol.lower()}_price_usd ', engine, index_col="index")
-    df_exchange_rate = pd.read_sql_query(f'SELECT * FROM public.src_usd_{currency.lower()}', engine, index_col="index")
+    df_price_usd = pd.read_sql_query(f'SELECT * FROM public.src_{symbol.lower()}_price_usd ', engine, index_col="date")
+    df_exchange_rate = pd.read_sql_query(f'SELECT * FROM public.src_usd_{currency.lower()}', engine, index_col="date")
 
     df_price_usd.drop(['1. open', '2. high', '3. low', '5. adjusted close',
                        '6. volume', '7. dividend amount', '8. split coefficient'],
@@ -70,7 +140,7 @@ def calc_load_daily_price_other_ccy():
     df_price_other_ccy.to_sql(f'prd_{symbol.lower()}_price_{currency.lower()}', engine, if_exists='replace', index=True)
 
 
-with DAG(dag_id="alphavantage_etl_dag", schedule_interval="*/5 * * * *", start_date=datetime(2022, 12, 1),
+with DAG(dag_id="alphavantage_etl_dag", schedule_interval="0 0 * * 2-6", start_date=datetime(2022, 12, 1),
          catchup=False, tags=["alphavantage"]) as dag:
 
     symbol = "SPY"
